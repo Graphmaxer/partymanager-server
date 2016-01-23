ece5d7c0c400ad2d62a1623351216b541edecb1b
Maxime Lambotin <Maxime Lambotin>
2016-01-23 18:17:27 +0100

    server.js changed again

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
-var express = require('express');  
 var app = express();
-var server = require('http').createServer(app);
-var io = require("socket.io").listen(server);
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
-app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+var osipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var osport = process.env.OPENSHIFT_NODEJS_PORT;
+
+app.set('port', osport || 3000);
+app.set('ipaddress', osipaddress);
+...
+
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket){ 
+    socket.emit('news', { hello: 'world' });
+        socket.on('my other event', function (data) {
+            console.log(data);
+        });
+        //some more code here
+});
+
+server.listen(app.get('port'), app.get('ipaddress'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
